2a0ea83af6e692bebca8cc30ae814a18b2f3a86c
Cameron Zwane <159318912+cameron-zwane@users.noreply.github.com>
2024-03-07 11:34:52 +0200

    feature(scripts): Fixed naming convention for scripts (#4)
    * feature(scripts): Fixed naming convention for scripts

--- Database Scripts/V011__create_table_enemy_difficulty_statistic.sql
+++ Database Scripts/V011__create_table_enemy_difficulty_statistic.sql
@@ -4,10 +4,10 @@ GO
 CREATE TABLE [dbo].[EnemyDifficultyStatistic](
 	[enemyDifficultyStatiticId] [INT] IDENTITY(1,1) NOT NULL,
 	[statisticTypeId] [INT] NOT NULL,
-	[enemyDifficultyID] [INT] NOT NULL,
+	[enemyDifficultyId] [INT] NOT NULL,
 	[points] [INT] NOT NULL,
 	PRIMARY KEY CLUSTERED (enemyDifficultyStatiticId),
 	FOREIGN KEY (statisticTypeId) REFERENCES [dbo].[StatisticType](statisticTypeId),
-	FOREIGN KEY (enemyDifficultyID) REFERENCES [dbo].[EnemyDifficulty](enemyDifficultyID)
+	FOREIGN KEY (enemyDifficultyId) REFERENCES [dbo].[EnemyDifficulty](enemyDifficultyId)
 );
 GO
